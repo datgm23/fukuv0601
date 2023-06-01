cf016209a5de3f580431ac20c7946d6a05d4adaf
YuTanaka <tanakaedu@users.noreply.github.com>
2023-06-01 16:03:51 +0900

    演習8-5

--- fukuv0601/Form1.cs
+++ fukuv0601/Form1.cs
@@ -19,20 +19,20 @@ namespace fukuv0601
 
             if (label1.Left < 0)
             {
-                vx = 10;
+                vx = Math.Abs(vx + (vx / 10));
             }
             else if (label1.Right > ClientSize.Width)
             {
-                vx = -10;
+                vx = -Math.Abs(vx + (vx / 10));
             }
 
             if (label1.Top < 0)
             {
-                vy = 10;
+                vy = Math.Abs(vy + (vy / 10));
             }
             else if (label1.Bottom > ClientSize.Height)
             {
-                vy = -10;
+                vy = -Math.Abs(vy + (vy / 10));
             }
 
             string t = label1.Text;
